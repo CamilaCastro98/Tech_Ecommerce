bb023fcafb62a125cb4c967a0ca7e6465611971c
Camila Castro <cami@Camilas-MacBook-Air.local>
2024-06-11 08:21:22 -0300

    Quitar apostrofes

--- src/app/contact/Contact.tsx
+++ src/app/contact/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
         <div className=" m-8 pb-4 border border-gray-50 rounded-md">
             <div className="bg-gray-50 w-full p-8">
                 <h1 className="text-lg text-p-color font-semibold mb-2">Contact Us!</h1>
-                <p className="text-p-color text-base mb-2">Reach out anytime. We're always here to assist with your tech solutions and inquiries</p>
+                <p className="text-p-color text-base mb-2">Reach out anytime. We are always here to assist with your tech solutions and inquiries</p>
             </div>
             <ul className="text-gray-50 flex flex-col gap-5 p-6">
                 <li className="flex gap-4 item-center">
